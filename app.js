4a2da076056c631ecc1c83bba0ed98f45e585f41
Ville Saarinen <ville.saarinen9@gmail.com>
2020-09-13 17:40:16 +0300

    osa4 4.1 4.2, init and refactor

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+
+const blogsRouter = require('./controllers/blogs')
+
+const app = express()
+
+mongoose.connect(config.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        logger.info('connected to MongoDB')
+    })
+    .catch((error) => {
+        logger.error('error connection to MongoDB:', error.message)
+    })
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+
+module.exports = app
---
+++ utils/middleware.js
@@ -0,0 +1,23 @@
+const logger = require('./logger')
+
+const unknownEndpoint = (req, res) => {
+    res.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (err, req, res, next) => {
+    logger.error(err.message)
+
+    if (err.name === 'CastError') {
+        return res.status(400).send({ error: 'malformatted id' })
+    }
+    else if (err.name === 'ValidationError') {
+        return res.status(400).json({ error: err.message })
+    }
+
+    next(err)
+}
+
+module.exports = {
+    unknownEndpoint,
+    errorHandler
+}
